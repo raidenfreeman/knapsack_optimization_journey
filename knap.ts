36b470316249918929e7f5e07e6c965f18b3026b
Konstantinos Gkinis <raidenfreeman@hotmail.com>
2023-07-02 20:56:23 +0200

    Function call overhead fixed

--- knap.ts
+++ knap.ts
@@ -1,4 +1,8 @@
 export function knap(items: readonly Item[], capacity: number): number {
+  return _knap(items, capacity);
+}
+
+function _knap(items: readonly Item[], capacity: number): number {
   if (items.length === 1) {
     const item = items[0];
     return item.w <= capacity ? item.v : 0;
@@ -7,10 +11,10 @@ export function knap(items: readonly Item[], capacity: number): number {
   const first = items[0]; // compare implementation with last
   const remaining = items.filter((_, i) => i > 0);
   if (first.w > capacity) {
-    return knap(remaining, capacity);
+    return _knap(remaining, capacity);
   }
-  const res = knap(remaining, capacity - first.w);
-  const res2 = knap(remaining, capacity);
+  const res = _knap(remaining, capacity - first.w);
+  const res2 = _knap(remaining, capacity);
   return Math.max(res + first.v, res2);
 }
 
