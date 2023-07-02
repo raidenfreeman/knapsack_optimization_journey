b5b9b3cc141d6a130302dd88f7e7f04cab7de4a8
Konstantinos Gkinis <raidenfreeman@hotmail.com>
2023-07-02 20:36:07 +0200

    Revert "function call overhead"
    This reverts commit a1ecf854069e7a958647c55986566a2e7b3123ad.

--- knap.ts
+++ knap.ts
@@ -1,8 +1,4 @@
 export function knap(items: readonly Item[], capacity: number): number {
-  return _knap(items, capacity);
-}
-
-const _knap = (items: readonly Item[], capacity: number): number => {
   if (items.length === 1) {
     const item = items[0];
     return item.w <= capacity ? item.v : 0;
@@ -16,11 +12,9 @@ const _knap = (items: readonly Item[], capacity: number): number => {
   const res = knap(remaining, capacity - first.w);
   const res2 = knap(remaining, capacity);
   return Math.max(res + first.v, res2);
-};
+}
 
 interface Item {
   w: number;
   v: number;
 }
-// // try map & object
-// const hm: Record<any, number> = {};
